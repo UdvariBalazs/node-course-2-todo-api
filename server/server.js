e6a58af0f4490e07fe58506a923184e4072bd427
Udvari Balázs <41227833+UdvariBalazs@users.noreply.github.com>
2018-11-05 10:38:52 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -26,6 +27,34 @@ app.get('/todos', (req, res) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+// GET /todos/1234324
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    
+    // Valid id is using isValid
+    if (!ObjectID.isValid(id)) {
+        // 404 - send back empty send
+        return res.status(404).send();
+    }
+
+    // findById
+    Todo.findById(id).then((todo) => {
+    // success
+        if (!todo) {
+            res.status(404 ).send();
+        }
+        else {
+            res.send({todo});
+            console.log('Unable to fin todo')
+        }
+
+    // error
+    }).catch((e) => {
+        // 400 - and send empty body back
+        res.status(400).send();
     });
 });
 
