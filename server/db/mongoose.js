521dee4091a5a23eb8ab17900ef07ae847976143
Udvari Balázs <41227833+UdvariBalazs@users.noreply.github.com>
2018-11-13 08:41:04 +0200

    Log the process.env.MONGODB_URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI/* || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }*/);
+console.log('------------- process.env.MONGODB_URI = ', process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI/* || 'mongodb://localhost:27017/TodoApp'*/), { useNewUrlParser: true };
+// Bongeszobol inditas: https://intense-stream-62044.herokuapp.com/
 
 module.exports = {mongoose};
