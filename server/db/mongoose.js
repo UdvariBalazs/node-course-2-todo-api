6de2e3fcd15ff9bb5d2ed4a3d74c62900fa8408f
Udvari Balázs <41227833+UdvariBalazs@users.noreply.github.com>
2018-11-17 13:59:29 +0200

    Add tests for PATCH /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-console.log('------------- process.env.MONGODB_URI = ', process.env.MONGODB_URI);
+//console.log('------------- process.env.MONGODB_URI = ', process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'), { useNewUrlParser: true };
 
 module.exports = {mongoose};
